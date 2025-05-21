133c8b43be1a99a6ce98c3e3f125407695e34eda
volkowlad <volkowladislav@gmail.com>
2025-05-21 12:13:59 +0300

    change pagination

--- internal/repos/repository.go
+++ internal/repos/repository.go
@@ -9,7 +9,7 @@ import (
 type Repository interface {
 	CreateTask(ctx context.Context, task TaskCreate) (uuid.UUID, error) // Создание задачи
 	GetTask(ctx context.Context, id uuid.UUID) (Task, error)
-	GetAllTasks(ctx context.Context) ([]Task, error)
+	GetAllTasks(ctx context.Context, page, limit int) ([]Task, error)
 	DeleteTask(ctx context.Context, id uuid.UUID) error
 	UpdateTask(ctx context.Context, task UpdateTask, id uuid.UUID) (Task, error)
 }
